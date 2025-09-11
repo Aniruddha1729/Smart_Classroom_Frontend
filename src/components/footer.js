56c7ab410eb929f63639e2d4b009c4db97507585
Krushnakant Patil <krushnakantpatil06@gmail.com>
2025-09-12 00:18:11 +0530

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>footer
+        <footer className="footer">
+            <p>&copy; 2024 My Website. All rights reserved.</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
