2e7a35fb0dbf9a2311e4950ad926155f0a60ec04
Krushnakant Patil <krushnakantpatil06@gmail.com>
2025-09-15 00:55:46 +0530

    Basic Login Page Created

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,27 @@
 import React from 'react'
 
-function navbar() {
+import './navbar.css'
+
+
+function Navbar() {
   return (
-    <div>
-        <nav className="navbar">
-            <h1>My Website</h1>
-            <ul className="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-    </div>
+    <nav className="navbar">
+   
+        <div className="navbar-logo">
+          <img src="logo.svg" alt="Logo" />
+          <h1>My Website</h1>
+        </div>
+     
+      <ul className="flex gap-6">
+        <ul className="navbar-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </ul>
+    </nav>
   )
 }
 
-export default navbar
+export default Navbar;
